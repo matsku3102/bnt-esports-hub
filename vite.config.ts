a99713ca0717b2b37f6192ae7d05f0e25d9c7fdb
Matias Kupila <matias.kupila@tuni.fi>
2025-12-01 22:23:24 +0100

    Set base path for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/bnt-esports-hub/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
