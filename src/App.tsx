407de103827c499df1ab2abdae1d761243a97ddf
Matias Kupila <matias.kupila@tuni.fi>
2025-12-01 22:51:07 +0100

    Fix React Router for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // just import Routes & Route
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Merch from "./pages/Merch";
@@ -17,17 +17,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/merch" element={<Merch />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/join" element={<JoinPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/merch" element={<Merch />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
